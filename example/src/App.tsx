fead84967a571d44e630e94b11ac7d113c1ab01c
Anders Blom Riecke <ar@mobicom-pro.com>
2024-09-20 15:17:58 +0200

    lint fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,14 @@
 import { View } from 'react-native';
-import Picker  from 'react-native-scrollable-time-date-picker';
+import Picker from 'react-native-scrollable-time-date-picker';
 
 export default function App() {
-
   const onConfirm = (date: string) => {
     console.log('from index', date);
-  }  
+  };
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', flexDirection: 'row', gap: 20, paddingHorizontal: 50}}>
-      <Picker 
+    <View>
+      <Picker
         type={'time'}
         confirmText={'Bekræft'}
         cancelText={'Annuler'}
@@ -17,10 +16,9 @@ export default function App() {
         value={'13:13'}
         open={false}
         onConfirm={(text) => onConfirm(text)}
-        buttonStyle={{paddingLeft: 25, padding: 10, borderRadius: 10, borderWidth: 1, borderColor: 'black'}}
         image={require('../assets/time_gray.png')}
       />
-      <Picker 
+      <Picker
         type={'date'}
         confirmText={'Bekræft'}
         cancelText={'Annuler'}
@@ -28,9 +26,8 @@ export default function App() {
         value={'18-09-2022'}
         open={false}
         onConfirm={(text) => onConfirm(text)}
-        buttonStyle={{paddingLeft: 25, padding: 10, borderRadius: 10, borderWidth: 1, borderColor: 'black'}}
         image={require('../assets/date_gray.png')}
-        language='da'
+        language="da"
       />
     </View>
   );
